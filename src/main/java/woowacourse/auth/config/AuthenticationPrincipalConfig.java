e232653cacd46631eca8c078dafdd17b4999dd2c
hyeonic <dev.hyeonic@gmail.com>
2022-06-06 02:33:31 +0900

    refactor: AuthenticationPrincipalConfig 로타입 수정

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -3,6 +3,7 @@ package woowacourse.auth.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
@@ -17,7 +18,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addArgumentResolvers(List argumentResolvers) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
     }
 
