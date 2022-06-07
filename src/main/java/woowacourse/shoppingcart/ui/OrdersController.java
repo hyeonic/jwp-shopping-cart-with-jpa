131575a9c5e7c499906b90e7abb42dd13cce00c8
hyeonic <dev.hyeonic@gmail.com>
2022-06-07 13:38:23 +0900

    feat: Orders 인수 테스트 요구사항에 맞게 변경

--- src/main/java/woowacourse/shoppingcart/ui/OrdersController.java
+++ src/main/java/woowacourse/shoppingcart/ui/OrdersController.java
@@ -4,17 +4,18 @@ import java.net.URI;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.shoppingcart.application.OrdersService;
+import woowacourse.shoppingcart.dto.customer.LoginCustomer;
 import woowacourse.shoppingcart.dto.orders.OrdersDetailRequest;
 import woowacourse.shoppingcart.dto.orders.OrdersResponse;
 
 @RestController
-@RequestMapping("/api/customers/{customerName}/orders")
+@RequestMapping("/api/customers/me/orders")
 public class OrdersController {
 
     private final OrdersService ordersService;
@@ -24,10 +25,10 @@ public class OrdersController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> addOrders(@PathVariable String customerName,
+    public ResponseEntity<Void> addOrders(@AuthenticationPrincipal LoginCustomer loginCustomer,
                                           @RequestBody @Valid List<OrdersDetailRequest> orderDetails) {
-        OrdersResponse ordersResponse = ordersService.save(orderDetails, customerName);
+        OrdersResponse ordersResponse = ordersService.save(orderDetails, loginCustomer.getUsername());
         return ResponseEntity.created(
-                URI.create("/api/" + customerName + "/orders/" + ordersResponse.getId())).build();
+                URI.create("/api/me/orders/" + ordersResponse.getId())).build();
     }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/OrdersAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/OrdersAcceptanceTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.dto.cartitem.CartItemSaveRequest;
-import woowacourse.shoppingcart.dto.customer.CustomerResponse;
 import woowacourse.shoppingcart.dto.customer.CustomerSaveRequest;
 import woowacourse.shoppingcart.dto.orders.OrdersDetailRequest;
 import woowacourse.shoppingcart.dto.product.ProductSaveRequest;
@@ -30,15 +29,14 @@ public class OrdersAcceptanceTest extends AcceptanceTest {
     void addOrders() {
         generateCustomer(MAT_SAVE_REQUEST);
         String accessToken = generateToken(new TokenRequest(MAT_USERNAME, MAT_PASSWORD));
-        CustomerResponse customerResponse = findCustomer(accessToken);
 
         Long productId1 = generateProduct(ONE_PRODUCT_SAVE_REQUEST);
         Long productId2 = generateProduct(TWO_PRODUCT_SAVE_REQUEST);
 
         CartItemSaveRequest cartItemSaveRequest1 = new CartItemSaveRequest(productId1, 2);
         CartItemSaveRequest cartItemSaveRequest2 = new CartItemSaveRequest(productId2, 3);
-        Long cartItemId1 = generateCartItem(customerResponse, cartItemSaveRequest1);
-        Long cartItemId2 = generateCartItem(customerResponse, cartItemSaveRequest2);
+        Long cartItemId1 = generateCartItem(accessToken, cartItemSaveRequest1);
+        Long cartItemId2 = generateCartItem(accessToken, cartItemSaveRequest2);
 
         List<OrdersDetailRequest> cartItemSaveRequests = List.of(
                 new OrdersDetailRequest(cartItemId1, 2),
@@ -47,8 +45,9 @@ public class OrdersAcceptanceTest extends AcceptanceTest {
 
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessToken)
                 .body(cartItemSaveRequests)
-                .when().post("/api/customers/{customerName}/orders", customerResponse.getUsername())
+                .when().post("/api/customers/me/orders")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value())
                 .extract();
@@ -75,17 +74,6 @@ public class OrdersAcceptanceTest extends AcceptanceTest {
                 .getAccessToken();
     }
 
-    private CustomerResponse findCustomer(String accessToken) {
-        return RestAssured.given().log().all()
-                .auth().oauth2(accessToken)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/api/customers/me")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .as(CustomerResponse.class);
-    }
-
     private Long generateProduct(ProductSaveRequest request) {
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -97,11 +85,12 @@ public class OrdersAcceptanceTest extends AcceptanceTest {
         return Long.parseLong(response.header("Location").split("/products/")[1]);
     }
 
-    private Long generateCartItem(CustomerResponse customerResponse, CartItemSaveRequest cartItemSaveRequest) {
+    private Long generateCartItem(String token, CartItemSaveRequest cartItemSaveRequest) {
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(token)
                 .body(cartItemSaveRequest)
-                .when().post("/api/customers/{customerName}/cartItems", customerResponse.getUsername())
+                .when().post("/api/customers/me/cartItems")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value())
                 .extract();
