4689b768c19dcc70e9bff46e35d12e3708e56004
hyeonic <dev.hyeonic@gmail.com>
2022-06-10 00:29:20 +0900

    feat: Product 생성을 위한 validation 추가

--- src/main/java/woowacourse/shoppingcart/dto/product/ProductSaveRequest.java
+++ src/main/java/woowacourse/shoppingcart/dto/product/ProductSaveRequest.java
@@ -1,12 +1,21 @@
 package woowacourse.shoppingcart.dto.product;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import woowacourse.shoppingcart.domain.Product;
 
 public class ProductSaveRequest {
 
+    @Size(min = 1, max = 100)
     private String name;
+
+    @Min(0)
     private int price;
+
+    @Size(max = 1024)
     private String imageUrl;
+
+    @Size(max = 255)
     private String description;
 
     public ProductSaveRequest(String name, int price, String imageUrl, String description) {
